b46ae4985bbe5d2de41b1210f5b61b97b7532a9d
Marshalldt1 <leonardotera92@gmail.com>
2022-10-07 13:16:15 -0300

    First test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+
+// catalogo hidden
+const buttonCatalogo = document.querySelector(".button-catalogo");
+const catalogo = document.querySelector(".catalogo-valores");
+
+buttonCatalogo.addEventListener("click", () => {
+  catalogo.classList.toggle("hidden-catalogo");
+});
+
 //  scroll menu suave
 function initScrollSuave() {
   const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
@@ -22,12 +31,12 @@ initScrollSuave();
 function initAnimacaoScroll() {
   const sections = document.querySelectorAll(".js-scroll");
   if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
+    const windowMetade = window.innerHeight * 1;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = sectionTop - windowMetade < 0;
+        const isSectionVisible = sectionTop - windowMetade < 0.5;
         if (isSectionVisible) section.classList.add("ativo");
       });
     }
